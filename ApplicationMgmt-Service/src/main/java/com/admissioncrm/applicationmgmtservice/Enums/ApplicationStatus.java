6253118e7b3719da6dfa70c0e1eeda3780fb179a
Dhuppatel <dhuppatel12@gmail.com>
2025-06-15 22:35:39 +0530

    implimented basic application service

---
+++ ApplicationMgmt-Service/src/main/java/com/admissioncrm/applicationmgmtservice/Enums/ApplicationStatus.java
@@ -0,0 +1,10 @@
+package com.admissioncrm.applicationmgmtservice.Enums;
+
+public  enum ApplicationStatus {
+    SUBMITTED,
+    UNDER_REVIEW,
+    APPROVED,
+    REJECTED,
+    WAITLISTED,
+    ADMITTED
+}
---
+++ ApplicationMgmt-Service/src/main/java/com/admissioncrm/applicationmgmtservice/Utills/JwtUtils.java
@@ -0,0 +1,49 @@
+package com.admissioncrm.applicationmgmtservice.Utills;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    private static final String SECRET_KEY = "1f1dada12ecf13b34cba99d384dc943dfbcbcdb25e6e21fbd37aeedffbcf1c88";
+
+    private SecretKey getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            return !isTokenExpired(token);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public String extractRole(String token) {
+        return extractAllClaims(token).get("role", String.class); // Assumes role is serialized as String
+    }
+}
